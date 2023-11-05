c64cf14c4d5a21fd8d9c34df6de3130a75dcdb97
Tyuiu.PyanzinaMA@outlook.com <mrpnzn@yaandex.ru>
2023-11-05 22:21:50 +0500

    Task3

---
+++ Tyuiu.PyanzinaMA.Sprint4.Task3.V29.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.PyanzinaMA.Sprint4.Task3.V29.Lib;
+
+namespace Tyuiu.PyanzinaMA.Sprint4.Task3.V29.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+
+            int[,] x = { { 2, 1, 6 },
+                          { 2, 4, 2 },
+                          { 5, 3, 1} };
+            Assert.AreEqual(20, ds.Calculate(x));
+        }
+    }
+}
---
+++ Tyuiu.PyanzinaMA.Sprint4.Task3.V29/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.PyanzinaMA.Sprint4.Task3.V29.Lib;
+
+namespace Tyuiu.PyanzinaMA.Sprint4.Task3.V29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            
+            Console.Title = "Спринт #4 | Выполнила: Пьянзина М. А. | ИИПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы. (статический ввод)                             *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #29                                                             *");
+            Console.WriteLine("* Выполнила: Пьянзина Мария Алексеевна | ИИПб-23-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* статическими значениями в диапазоне от 5 до 9. Найдите произведение     *");
+            Console.WriteLine("* элементов в первом столбце массива.                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            
+            int[,] Array = { { 9, 9, 8, 6, 9}, { 5, 8, 8, 8, 7 }, { 6, 5, 9, 7, 9 }, { 7, 7, 9, 7, 8 }, { 8, 5, 8, 5, 5 } };
+
+            Console.Write("Матрица:\n 9 9 8 6 9 \n 5 8 8 8 7 \n 6 5 9 7 9 \n 7 7 9 7 8 \n 8 5 8 5 5 ");
+
+            Console.WriteLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine($"Произведение элементов в первом столбце матрицы = {ds.Calculate(Array)}");
+            Console.ReadKey();
+        }
+    }
+}
