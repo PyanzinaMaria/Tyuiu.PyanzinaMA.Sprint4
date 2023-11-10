bbe7618bffb9906638d22344b2fb8067e61cc120
Tyuiu.PyanzinaMA@outlook.com <mrpnzn@yaandex.ru>
2023-11-10 20:12:37 +0500

    Task4

---
+++ Tyuiu.PyanzinaMA.Sprint4.Task4.V27.Test/DataSeviceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.PyanzinaMA.Sprint4.Task4.V27.Lib;
+
+namespace Tyuiu.PyanzinaMA.Sprint4.Task4.V27.Test
+{
+    [TestClass]
+    public class DataSeviceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+
+            int[,] Array = new int[,] { { 1,5,3 }, { 6,2,8 }, { 7,4,2 } };
+
+            Assert.AreEqual(16, ds.Calculate(Array));
+        }
+    }
+}
---
+++ Tyuiu.PyanzinaMA.Sprint4.Task4.V27.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PyanzinaMA.Sprint4.Task4.V27.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PyanzinaMA.Sprint4.Task4.V27.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2033a3eb-34b9-4bd3-ba77-6f687ae424fd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
